44436dfce6abf2ca9e0ae815952ce8f3733a6c7d
Zhenya <subbotinjon47@gmail.ru>
2020-06-28 19:17:20 +0300

    added templates

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const data = ['hello', 'world', '!']
  
 app.get('/', function (req, res) {
-  res.send('Hello World!!!')
+  res.render('index', {data})
+})
+
+app.get('/create', function (req, res) {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    data.push(req.body.text)
+    res.redirect('/')
 })
  
 app.listen(3000)
