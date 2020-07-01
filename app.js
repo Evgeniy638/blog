34e828164cf0421990a6b3164f2f33866115eeb7
Evgeniy <Evgeniy>
2020-07-01 17:25:23 +0300

    env

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const staticAsset = require('static-asset');
 
-const Post = require('./models/post')
+const app = express();
 
-const app = express()
-
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(staticAsset(__dirname + "/public") );
-app.use(express.static(path.join(__dirname, '/public')))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(staticAsset(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  'javascripts', 
+  '/javascripts',
   express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
-)
+);
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
-module.exports = app
+module.exports = app;
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssnano = require('gulp-cssnano')
 const plumber = require('gulp-plumber');
+const concat = require('gulp-concat')
+const uglify = require('gulp-uglify')
 
 const scss = () => {
     return gulp.src('dev/scss/**/*.scss')
@@ -15,12 +17,20 @@ const scss = () => {
     .pipe(gulp.dest('./public/stylesheets'))
 }
 
-const defaultTask = (cb) => {
-    scss()
-
-    //gulp.watch('dev/scss/**/*.scss', scss)
-
-    cb()
+const scripts = () => {
+    return gulp.src(['dev/js/auth.js'])
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/javascripts'))
 }
 
-exports.default = defaultTask
+const defaultTask = (cd) => {
+    gulp.parallel(scss, scripts)
+
+    //gulp.watch('dev/js/**/*.js', scripts)
+    //gulp.watch('dev/scss/**/*.scss', scss)
+
+    return cd()
+}
+
+exports.default = gulp.parallel(scss, scripts)
